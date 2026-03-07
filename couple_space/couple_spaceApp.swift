bbf31444f5be595b273d1bb3b2d185d80c643a15
custodeemanuel <emanuel.custode@icloud.com>
2026-03-07 12:34:11 +0100

    Initial Commit

---
+++ couple_space/couple_spaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  couple_spaceApp.swift
+//  couple_space
+//
+//  Created by Emanuel on 07.03.26.
+//
+
+import SwiftUI
+
+@main
+struct couple_spaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
